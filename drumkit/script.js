fe66d860759e74ec870bc91ff5561439f634f943
Bindiya <bindiya@quintype.com>
2019-08-15 08:18:51 +0530

    CSS modification

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,16 +1,16 @@
-  function playMusic(e) {
-    const music = document.querySelector(`music[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!music) return;
-    key.classList.add('playing');
-    music.currentTime = 0;
-    music.play();
-  }
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+function playMusic(e) {
+  const music = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!music) return;
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playMusic);
+  key.classList.add('playing');
+  music.currentTime = 0;
+  music.play();
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playMusic);
